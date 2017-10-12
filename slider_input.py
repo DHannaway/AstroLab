72182fa44831a9ed0bebdda3994243c43dda7e05
DHannaway <danielhannaway95@gmail.com>
2017-10-12 17:05:18 +0100

    Output observed data to compare with model image, also plot intensity offset along vertical lines

--- slider_input.py
+++ slider_input.py
@@ -4,6 +4,13 @@ import matplotlib as mpl
 import os
 from tkinter import *
 from cdict import *
+from astropy.io import fits
+import shutil
+
+#Delete the old 'images' folder, and create a new one
+if os.path.exists('images'):
+	shutil.rmtree('images')
+os.makedirs('images')
 
 #Path to the executable file:
 codepath='C:\\Users\\Dan\\Documents\\GitHub\\Python\\'
@@ -17,7 +24,22 @@ for i in range(0,len(cmap1)):
 	cdict1.append(arr)
 cm = mpl.colors.ListedColormap(cdict1)
 
+fitsfile = fits.open('NGC891_V_data.fits')
+fitsdata = fitsfile[0].data
+
+datalist=[]
+for i in range(355,555):
+	arr=[]
+	for j in range(250,650):
+		arr.append(fitsdata[i][j])
+	datalist.append(arr)
+dataplot=np.array(datalist)
+tdata=np.transpose(dataplot)
+nloop=1
+
 def simulation():
+	global nloop
+
 	B_T=w1.get()
 	h_stars=w2.get()
 	r_stars=w3.get()
@@ -25,7 +47,7 @@ def simulation():
 	r_ISM=w5.get()
 	n_0=w6.get()
 
-	nphotons=100000
+	nphotons=600000
 
 	#Replace the old .param file with a new one with the entered values
 	with open('test_dustsimulation_original.param') as g:
@@ -54,34 +76,64 @@ def simulation():
 	#Input this new file into the testDustSimulation program and compile
 	os.system(codepath+filename)  
 
-	#Input the created binary file, and plot as an image
+	#Input the created binary file, and plot as an image 
+	# Compare this image with the FITS data
 	image = np.fromfile("test_dustsimulation_output.dat", dtype = np.float64)
 	image = image.reshape((200, 200))
-	fig, ax = plt.subplots() 
-#	ax.matshow(np.transpose(image), cmap='Greys_r')
-	ax.matshow(np.transpose(image), cmap=cm)
-	plt.text(5,60,"B_T ="+str(B_T)+'\n'+"h_stars ="+str(h_stars)+" kpc"+'\n'+"r_stars ="+str(r_stars)+" kpc"+'\n'+"h_ISM ="+str(h_ISM)+" kpc"+'\n'+"r_ISM ="+str(r_ISM)+" kpc"+'\n'+"n0 ="+str(n_0)+" cm^-3",color='w')
 
-	tick_locs_x = [0,100,200]
-	tick_locs_y = [0,100,200]
+	image2=[]
+	for i in range(0,len(image)):
+		arr=[]
+		for j in range(50,150):
+			arr.append(image[i][j])
+		image2.append(arr)
+	image2 = np.kron(image2,np.ones((2,2)))
 
-	xtick_lbls = ['-12.1kpc','0','12.1kpc']
-	ytick_lbls = ['-12.1kpc','0','12.1kpc']
 
-	plt.xticks(tick_locs_x, xtick_lbls,rotation=0,fontsize=10)
-	plt.yticks(tick_locs_y, ytick_lbls,rotation=0,fontsize=10)
+	fig, ax = plt.subplots(2) 
+	ax[0].matshow(dataplot, cmap=cm)
+	ax[1].matshow(np.transpose(image2), cmap=cm)
+
+	plt.text(420,50,"B_T ="+str(B_T)+'\n'+"h_stars ="+str(h_stars)+" kpc"+'\n'+"r_stars ="+str(r_stars)+" kpc"+'\n'+"h_ISM ="+str(h_ISM)+" kpc"+'\n'+"r_ISM ="+str(r_ISM)+" kpc"+'\n'+"n0 ="+str(n_0)+" cm^-3",color='k')
+#	tick_locs_x = [0,200,400]
+#	tick_locs_y = [0,200,400]
+
+#	xtick_lbls = ['-12.1kpc','0','12.1kpc']
+#	ytick_lbls = ['-12.1kpc','0','12.1kpc']
+
+#	plt.xticks(tick_locs_x, xtick_lbls,rotation=0,fontsize=10)
+#	plt.yticks(tick_locs_y, ytick_lbls,rotation=0,fontsize=10)
+
+	linearray=[150,200,340]
+	N=len(linearray)
+	color=iter(plt.cm.rainbow(np.linspace(0,1,N)))
+	for i in range(0,len(linearray)):
+		c=next(color)
+		ax[0].axvline(linearray[i],c=c)
+		ax[1].axvline(linearray[i],c=c)
+
+	plt.savefig('images/image'+str(nloop))
 	plt.show()
 	plt.close()
-	plt.plot(image[75],c='blue',label='75')
-	plt.plot(image[100],c='green',label='100')
-	plt.plot(image[140],c='red',label='150')
 
-	plt.legend(loc="upper left")
+	def normplot(func,n):
+	#	return (func[n]/max(func[n]))-(min(func[n])/max(func[n]))
+		return (func[n]/(func[n][100]))-(min(func[n])/(func[n][100]))
+
+	color=iter(plt.cm.rainbow(np.linspace(0,1,N)))
+
+	fig,ax=plt.subplots(len(linearray),sharey=False)
+	for i in range (0,len(linearray)):
+		ax[i].plot(normplot(image2,linearray[i]),linestyle='--',label='model',c='k')
+		ax[i].plot(normplot(tdata,linearray[i]),label='data',c=next(color))
+		ax[i].set_title("Intensity offset plot for x = "+str(linearray[i]))
+		ax[i].legend(loc='upper left')
+	plt.tight_layout()
+	plt.savefig('images/offset'+str(nloop))
 	plt.show()
-
-
+	nloop=nloop+1
 master = Tk()
-w1 = Scale(master, from_=0.0, to=1.0,resolution=0.01, orient=HORIZONTAL,length=500,label="B/T")
+w1 = Scale(master, from_=0.0, to=0.99,resolution=0.01, orient=HORIZONTAL,length=500,label="B/T")
 w1.pack()
 w2 = Scale(master, from_=0.0, to=2.0,resolution=0.01, orient=HORIZONTAL,length=500,label="h_stars [kpc]")
 w2.pack()
